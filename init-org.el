7e70183afd5c5fbc4033d7ee93ada0302da65047
dongxiong <chdongxiong@gmail.com>
2013-09-18 14:06:46 +0800

    增加url到.gitignore，修改org配置

--- init-org.el
+++ init-org.el
@@ -4,33 +4,44 @@
 
 (require 'ox-publish)
 
-(setq org-html-postamble-format (quote (("cn" "<p class=\"author\">作者： %a</p>
-<p class=\"date\">日期： %d</p>"))))
+(setq org-html-postamble-format (quote (("zh-CN" "<p class=\"license\">本文采用 <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/3.0/cn/\">知识共享署名-相同方式共享 3.0 中国大陆许可协议</a> 进行许可</p><p class=\"date\">%d</p>"))))
+(setq org-html-preamble nil)
+(setq org-html-postamble nil)
+
+
+;; 生成的html不附上xml声明
+(setq org-html-xml-declaration nil)
 
 (setq org-publish-project-alist
       '(
-	("org-notes"
-	 :author "求其"
-	 :language "cn"
-	 :base-directory "~/org/"
-	 :base-extension "org"
-	 :publishing-directory "~/public_html/"
-	 :recursive t
-	 :publishing-function org-html-publish-to-html
-	 :headline-levels 4             ; Just the default for this project.
-	 :auto-sitemap t
-	 :html-head-include-default-style nil
-	 :html-head-include-scripts nil
-	 :html-postamble t
-	 )
-	("org-static"
-	 :base-directory "~/org/"
-	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-	 :publishing-directory "~/public_html/"
-	 :recursive t
-	 :publishing-function org-publish-attachment
-	 )
-	("org" :components ("org-notes" "org-static"))
+        ("org"
+         :author "dongxiong"
+         :language "zh-CN"
+         :base-directory "~/Dropbox/blog/org/"
+         :base-extension "org"
+         :sitemap-filename "sitemap.org"
+         :sitemap-title "Sitemap"
+         :sitemap-sort-files "anti-chronologically"
+         :sitemap-file-entry-format "%t"
+         :publishing-directory "~/Dropbox/blog/html/"
+         :publishing-function org-html-publish-to-html
+         :recursive t
+         :headline-levels 4
+         :auto-sitemap t
+         :html-link-up "/sitemap.html"
+         :html-link-home "/"
+         :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"/style.css\" />"
+         :html-head-include-default-style nil
+         :html-head-include-scripts nil
+         )
+        ("static"
+         :base-directory "~/Dropbox/blog/org/"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|html"
+         :publishing-directory "~/Dropbox/blog/html/"
+         :recursive t
+         :publishing-function org-publish-attachment
+         )
+        ("blog" :components ("org" "static"))
         ))
 
 (provide 'init-org)
