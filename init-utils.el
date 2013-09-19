973650724aa4a71b4a5af71d7225d633722fac5b
dongxiong <chdongxiong@gmail.com>
2013-09-19 12:24:42 +0800

    增加两个有用的函数，删除和重命名当前文件和buffer

--- init-utils.el
+++ init-utils.el
@@ -26,3 +26,29 @@ BUFFER may be either a buffer or its name (a string)."
       (error "Cannot kill buffer.  Not a live buffer: `%s'" buffer))))
 
 (provide 'init-utils)
+
+;; 删除当前文件及buffer
+(defun delete-this-file ()
+  "Delete the current file, and kill the buffer."
+  (interactive)
+  (or (buffer-file-name) (error "No file is currently being edited"))
+  (when (yes-or-no-p (format "Really delete '%s'?"
+                             (file-name-nondirectory buffer-file-name)))
+    (delete-file (buffer-file-name))
+    (kill-this-buffer)))
+
+;; 重命名当前文件及buffer
+(defun rename-this-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (unless filename
+      (error "Buffer '%s' is not visiting a file!" name))
+    (if (get-buffer new-name)
+        (message "A buffer named '%s' already exists!" new-name)
+      (progn
+        (rename-file filename new-name 1)
+        (rename-buffer new-name)
+        (set-visited-file-name new-name)
+        (set-buffer-modified-p nil)))))
