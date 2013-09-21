17dbd601b2173e556181e824c41ce5b43571c4b0
dongxiong <chdongxiong@gmail.com>
2013-09-21 21:11:56 +0800

    字体大小设置为14时，有些小问题

--- init-gui-frames.el
+++ init-gui-frames.el
@@ -1,6 +1,9 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
+;; 这里有一个问题，字体大小设置为14时，undo-tree的buffer将会一直出现在当前buffer下面
+;; 而不会出现在当前buffer右边，而字体大小设置为12即默认大小时，undo-tree的buffer将会
+;; “随机应变”，其它会出现临时buffer的情况都会存在该问题，所幸问题不影响正常使用
 (set-default-font "Source Code Pro-14")
 (set-fontset-font "fontset-default" 'gb18030' ("STHeiti" . "unicode-bmp"))
 
