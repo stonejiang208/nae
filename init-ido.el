b129d7654b745e2e58bbb9a8ef7cebb662686c15
dongxiong <chdongxiong@gmail.com>
2013-09-21 23:37:35 +0800

    ido-ubiquitous，很好很强大，另外开启ido-use-virtual-buffers

--- init-ido.el
+++ init-ido.el
@@ -1,7 +1,10 @@
 ;; 注：ido-better-flex会导致smex补全变慢
-
 (ido-mode t)
 (setq ido-enable-flex-matching t)
+(setq ido-use-virtual-buffers t)
+
+(require-package 'ido-ubiquitous)
+(ido-ubiquitous-mode t)
 
 (require-package 'smex)
 
