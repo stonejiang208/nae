71e032558cd1b8adc501129c5ed67a160cfb8c09
dongxiong <chdongxiong@gmail.com>
2013-09-22 22:05:49 +0800

    增加php manual

--- init-php.el
+++ init-php.el
@@ -15,7 +15,8 @@ For detail, see `comment-dwim'."
 ;; 更改php注释类型
 (add-hook 'php-mode-hook
           (lambda ()
-            (define-key php-mode-map [remap comment-dwim] 'php-comment-dwim
-              )))
+            (define-key php-mode-map [remap comment-dwim] 'php-comment-dwim)
+            (setq php-manual-path "~/.emacs.d/php-manual/")
+              ))
 
 (provide 'init-php)
