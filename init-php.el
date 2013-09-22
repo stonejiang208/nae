64a9ddad107fff1a438e462d28bb1402206acf84
dongxiong <chdongxiong@gmail.com>
2013-09-22 12:47:36 +0800

    修改php注释类型

--- init-php.el
+++ init-php.el
@@ -2,4 +2,20 @@
 
 (setq php-mode-coding-style 'symfony2)
 
+(defun php-comment-dwim (arg)
+  "Comment or uncomment current line or region in a smart way.
+For detail, see `comment-dwim'."
+  (interactive "*P")
+  (require 'newcomment)
+  (let (
+        (comment-start "// ") (comment-end "")
+        )
+    (comment-dwim arg)))
+
+;; 更改php注释类型
+(add-hook 'php-mode-hook
+          (lambda ()
+            (define-key php-mode-map [remap comment-dwim] 'php-comment-dwim
+              )))
+
 (provide 'init-php)
