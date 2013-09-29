6c4bf88c3463158f04c7560911a9b6975e87c131
dongxiong <chdongxiong@gmail.com>
2013-09-29 12:03:50 +0800

    c-mode多行注释类型

--- init-c.el
+++ init-c.el
@@ -1,7 +1,13 @@
 (setq c-default-style "k&r"
       c-basic-offset 4)
 
-;; (add-hook 'c-mode-hook (lambda () (setq comment-start "//"
-;;                                     comment-end   "")))
+(add-hook 'c-mode-hook (lambda ()
+                         (setq comment-start "// " comment-end "")))
+
+(require-package 'smart-compile)
+
+(after-load 'smart-compile
+  (add-to-list 'smart-compile-alist
+               '("\\.c\\'"          . "gcc -Wall -O2 %f -lm -o %n")))
 
 (provide 'init-c)
