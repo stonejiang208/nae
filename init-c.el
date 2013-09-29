457069dcfb59c588940900648fe6234351f6e0ef
dongxiong <chdongxiong@gmail.com>
2013-09-29 12:04:45 +0800

    空格清理

--- init-c.el
+++ init-c.el
@@ -8,6 +8,6 @@
 
 (after-load 'smart-compile
   (add-to-list 'smart-compile-alist
-               '("\\.c\\'"          . "gcc -Wall -O2 %f -lm -o %n")))
+               '("\\.c\\'" . "gcc -Wall -O2 %f -lm -o %n")))
 
 (provide 'init-c)
