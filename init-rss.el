c8ae1df9a4d2e254f4378a552c1af44c5e3b8f15
dongxiong <chdongxiong@gmail.com>
2013-10-03 01:16:50 +0800

    更新rss源

--- init-rss.el
+++ init-rss.el
@@ -3,9 +3,12 @@
 (setq elfeed-feeds
       '("http://coolshell.cn/feed"
         "http://dlfkevin.com/feed"
+        "http://ergoemacs.org/emacs/blog.xml"
+        "http://www.guao.hk/feed"
         "http://hawklim.com/?feed=rss2"
         "http://iamhiko.com/feed"
         "http://segmentfault.com/feeds"
-        "http://v2ex.com/feed/tab/jobs.xml"))
+        "http://v2ex.com/feed/tab/jobs.xml"
+        "http://www.laruence.com/feed"))
 
 (provide 'init-rss)
