e31c635c9314492cee9bd85f7d91d20393016b62
dongxiong <chdongxiong@gmail.com>
2013-10-02 21:54:51 +0800

    rss订阅，很不错的mode

---
+++ init-rss.el
@@ -0,0 +1,11 @@
+(require-package 'elfeed)
+
+(setq elfeed-feeds
+      '("http://coolshell.cn/feed"
+        "http://dlfkevin.com/feed"
+        "http://hawklim.com/?feed=rss2"
+        "http://iamhiko.com/feed"
+        "http://segmentfault.com/feeds"
+        "http://v2ex.com/feed/tab/jobs.xml"))
+
+(provide 'init-rss)
