591068e07f1aa7d2fcd20cbf6b0331f69f178641
dongxiong <chdongxiong@gmail.com>
2013-10-03 13:44:24 +0800

    html文件也用web-mode编辑

--- init-web-mode.el
+++ init-web-mode.el
@@ -4,4 +4,7 @@
 (setq web-mode-script-padding 2)
 (setq web-mode-disable-auto-pairing t)
 
+;; html文件也用web-mode来编辑
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
 (provide 'init-web-mode)
