756015c6e39ce85c3c2a634faf47742f331349f8
dongxiong <chdongxiong@gmail.com>
2013-10-03 13:22:22 +0800

    解决auto-complete变慢的问题

--- init-shell.el
+++ init-shell.el
@@ -4,7 +4,8 @@
 
 ;; 值加上"-i"令通过M-!执行shell命令使用interactive shell，默认的值为"-c"
 ;; 因此M-!可以执行.bash_profile中的alias，alias只能在interactive shell中执行
-(setq shell-command-switch "-ic")
+;; 注意：http://stackoverflow.com/a/17595062，按以下设置会导致auto-complete变慢
+;; (setq shell-command-switch "-ic")
 
 ;; 清楚shell所有output
 (defun clear-shell ()
