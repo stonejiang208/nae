1d382f7cffd78d6a3ef1bdffe1d7c2b81cfd6b76
dongxiong <chdongxiong@gmail.com>
2013-10-03 14:01:47 +0800

    解决yasnippet在web-mode下展开自动缩进的问题

--- init-web-mode.el
+++ init-web-mode.el
@@ -3,6 +3,9 @@
 (setq web-mode-style-padding 2)
 (setq web-mode-script-padding 2)
 (setq web-mode-disable-auto-pairing t)
+;; 其实我也不知道这一项有什么用，不过将它设为空后
+;; yasnippet在web-mode下展开就会自动缩进了。
+(setq web-mode-indent-style nil)
 
 ;; html文件也用web-mode来编辑
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
