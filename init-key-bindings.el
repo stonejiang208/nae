cd2e99d779bfc23134dee6e526fc6af16730517c
dongxiong <chdongxiong@gmail.com>
2013-10-30 21:39:28 +0800

    去掉backward-delete-char的全局绑定

--- init-key-bindings.el
+++ init-key-bindings.el
@@ -12,9 +12,6 @@
 (global-set-key (kbd "M-.") 'helm-etags-select)
 (global-set-key (kbd "M-/") 'hippie-expand)
 (global-set-key (kbd "RET") 'newline-and-indent)
-;; 按<backspace>删除tab的时候删除整个tab，而不是先把tab转换成空格，再删除一个空格。
-;; 该键在一些mode默认绑定的函数是backward-delete-char-untabify
-(global-set-key (kbd "<backspace>") 'backward-delete-char)
 
 (global-set-key "\C-x2" 'split-window-below-and-focus)
 (global-set-key "\C-x3" 'split-window-right-and-focus)
