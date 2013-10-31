8ba1ca3a1b1897164cca02ddf6862bcfff1a45bc
dongxiong <chdongxiong@gmail.com>
2013-10-31 21:16:47 +0800

    get-bookmarks-in-current-window

--- init-bookmark.el
+++ init-bookmark.el
@@ -1,6 +1,14 @@
 (require-package 'bookmark+)
 
-; 更改书签后立即保存
+;; 更改书签后立即保存
 (setq bookmark-save-flag 1)
+
+;; 参考http://stackoverflow.com/questions/12023262/getting-bookmark-bmenu-list-into-a-new-frame
+;; 还不是很明白(bookmark-bmenu-list)不是应该在other window创建了一个*Bookmark List*吗，为什么
+;; 在current window调用switch-to-buffer切换到*Bookmark List*后other-window没有显示*Bookmark List*
+(defun get-bookmarks-in-current-window ()
+  (interactive)
+  (bookmark-bmenu-list)
+  (switch-to-buffer "*Bookmark List*"))
 
 (provide 'init-bookmark)
--- init-key-bindings.el
+++ init-key-bindings.el
@@ -9,6 +9,7 @@
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-x o") 'switch-window)
+(global-set-key (kbd "C-x r l") 'get-bookmarks-in-current-window)
 (global-set-key (kbd "M-.") 'helm-etags-select)
 (global-set-key (kbd "M-/") 'hippie-expand)
 (global-set-key (kbd "RET") 'newline-and-indent)
