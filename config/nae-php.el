4d2175585c86d8bf6419e2083ff60f175c415e4e
hellobear <hellobear@foxmail.com>
2014-01-03 00:29:16 +0800

    添加一个函数，一个新的nae-php-coding-style变量

--- config/nae-php.el
+++ config/nae-php.el
@@ -1,23 +1,24 @@
 (require-package 'php-mode)
 
 ;; 提供选择的coding style：codeigniter、drupal、pear、symfony2、wordpress
-;; (setq nae-php-mode-coding-style 'codeigniter)
-(setq php-mode-coding-style 'symfony2)
+(setq nae-php-mode-coding-style 'codeigniter)
 
-;; (cond ((memq my-php-mode-coding-style '(drupal pear symfony2 wordpress))
-;;        (setq php-mode-coding-style my-php-mode-coding-style))
-;;       ((eq my-php-mode-coding-style 'codeigniter)
-;;        (setq php-mode-coding-style 'pear)))
+(cond ((memq nae-php-mode-coding-style '(drupal pear symfony2 wordpress))
+       (setq php-mode-coding-style nae-php-mode-coding-style))
+      ((eq nae-php-mode-coding-style 'codeigniter)
+       (setq php-mode-coding-style 'pear)))
 
-;; (defun check-php-coding-style (styles)
-;;   (and (boundp 'my-php-mode-coding-style)
-;;        (memq my-php-mode-coding-style styles)))
+(defun nae-in-php-coding-styles (styles)
+  (and (boundp 'nae-php-mode-coding-style)
+       (memq nae-php-mode-coding-style styles)))
 
-;; (check-php-coding-style '(pear symfony2))
+(nae-in-php-coding-styles '(pear codeigniter))
 
 ;; 更改php注释类型
 (add-hook 'php-mode-hook
           (lambda ()
+            (cond ((eq nae-php-mode-coding-style 'codeigniter)
+                   (yas-activate-extra-mode 'codeigniter-mode)))
             (setq php-manual-path "~/.emacs.d/php-manual/")
             ;; 避免与global-set-key的冲突
             (local-unset-key (kbd "C-."))
