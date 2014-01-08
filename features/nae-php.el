7b9e55cec3a4fc21caceef3d008cea7c426bf19d
hellobear <ihellobear@gmail.com>
2014-01-09 00:15:40 +0800

    添加了codeigniter coding style

--- features/nae-php.el
+++ features/nae-php.el
@@ -14,7 +14,31 @@
   (and (boundp 'nae-php-mode-coding-style)
        (memq nae-php-mode-coding-style styles)))
 
-(nae-in-php-coding-styles '(pear codeigniter))
+;; 添加CodeIgniter Coding Style
+(c-add-style
+ "codeigniter"
+ '((c-basic-offset . 4)
+   (c-offsets-alist . (
+					   (topmost-intro-cont . (first c-lineup-cascaded-calls
+                                                    php-lineup-arglist-intro))
+					   (substatement-open . 0)
+					   (case-label . +)
+                       (arglist-close . php-lineup-arglist-close)
+                       (arglist-intro . php-lineup-arglist-intro)
+                       (arglist-cont-nonempty . +)
+                       (knr-argdecl . [0])
+                       (statement-cont . (first c-lineup-cascaded-calls +))))))
+
+(defun nae-php-enable-codeigniter-coding-style ()
+  "Makes php-mode use coding styles that are preferable for
+working with CodeIgniter."
+  (interactive)
+  (setq indent-tabs-mode t
+		fill-column 78
+		tab-width 4
+		;; 下面这个我也不太清楚是干嘛的，参考php-mode里面的配置
+		c-indent-comments-syntactically-p t)
+  (c-set-style "codeigniter"))
 
 ;; 更改php注释类型
 (add-hook 'php-mode-hook
@@ -26,8 +50,5 @@
             (setq php-manual-path "~/.emacs.d/php-manual/")
             ;; 避免与global-set-key的冲突
             (local-unset-key (kbd "C-."))
-            ;; 遇到ac-stop-words时不弹出auto-complete菜单
-            (make-local-variable 'ac-stop-words)
-            (add-to-list 'ac-stop-words "php")))
 
 (provide 'nae-php)
