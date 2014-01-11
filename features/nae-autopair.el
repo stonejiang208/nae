d3762b6a5b222e4e0c9ad2fd7200e6b2c46c3a3e
hellobear <ihellobear@gmail.com>
2014-01-11 10:14:54 +0800

    括号补全

--- features/nae-autopair.el
+++ features/nae-autopair.el
@@ -3,4 +3,12 @@
 (autopair-global-mode)
 (setq autopair-blink nil)
 
+;; emacs lisp mode中注释和字符串中输入 ` 后补全 '
+(add-hook 'emacs-lisp-mode-hook
+          #'(lambda ()
+              (push '(?` . ?')
+                    (getf autopair-extra-pairs :comment))
+              (push '(?` . ?')
+                    (getf autopair-extra-pairs :string))))
+
 (provide 'nae-autopair)
