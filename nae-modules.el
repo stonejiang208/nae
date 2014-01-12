b101ae613e416824d4ac36852b2a402056da4938
hellobear <ihellobear@gmail.com>
2014-01-12 14:34:35 +0800

    将require语句移到nae-modules.el中

---
+++ nae-modules.el
@@ -0,0 +1,54 @@
+(require 'nae-utils)
+(require 'nae-packages)
+(require 'nae-themes)
+;; (require 'nae-exec-path)
+
+(require 'nae-gui-frames)
+
+(require 'nae-tramp)
+
+(require 'nae-dired)
+(require 'nae-ibuffer)
+(require 'nae-isearch)
+(require 'nae-projectile)
+(require 'nae-flycheck)
+;; (require 'nae-flyspell)
+(require 'nae-compile)
+(require 'nae-yasnippet)
+(require 'nae-auto-complete)
+(require 'nae-helm)
+
+(require 'nae-ido)
+(require 'nae-bookmark)
+(require 'nae-hippie-expand)
+(require 'nae-windows)
+(require 'nae-sessions)
+(require 'nae-git)
+(require 'nae-vi)
+
+(require 'nae-autopair)
+(require 'nae-editing-utils)
+
+(require 'nae-web-mode)
+(require 'nae-c)
+(require 'nae-php)
+(require 'nae-org)
+(require 'nae-slime)
+(require 'nae-emmet)
+(require 'nae-vimrc-mode)
+(require 'nae-thrift)
+
+(require 'nae-shell)
+;; (require 'nae-w3m)
+(require 'nae-rss)
+(require 'nae-douban-fm)
+(require 'nae-calendar)
+
+(require 'nae-misc)
+(require 'nae-dir-locals)
+
+(require 'nae-key-bindings)
+
+(require 'nae-hack)
+
+(require 'nae-locales)
