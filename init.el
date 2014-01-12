0bfb92b0e555efa2b8b33f95567069459fe4050b
hellobear <ihellobear@gmail.com>
2014-01-12 15:10:05 +0800

    调整local文件名

--- init.el
+++ init.el
@@ -4,9 +4,11 @@
 (defvar nae-vendor-dir (expand-file-name  "vendor" nae-dir))
 (defvar nae-custom-dir (expand-file-name "custom" nae-dir))
 
+(add-to-list 'load-path nae-dir)
 (add-to-list 'load-path nae-core-dir)
 (add-to-list 'load-path nae-modules-dir)
 (add-to-list 'load-path nae-vendor-dir)
+(add-to-list 'load-path nae-custom-dir)
 
 (defconst *is-a-mac* (eq system-type 'darwin))
 (defconst *is-gui* (if window-system t nil))
@@ -14,60 +16,7 @@
 ;; 彩蛋
 (defconst wq! "Damn you! I am not vi!  Use C-x C-c instead.")
 
-(require 'nae-utils)
-(require 'nae-packages)
-(require 'nae-themes)
-;; (require 'nae-exec-path)
-
-(require 'nae-gui-frames)
-
-(require 'nae-tramp)
-
-(require 'nae-dired)
-(require 'nae-ibuffer)
-(require 'nae-isearch)
-(require 'nae-projectile)
-(require 'nae-flycheck)
-;; (require 'nae-flyspell)
-(require 'nae-compile)
-(require 'nae-yasnippet)
-(require 'nae-auto-complete)
-(require 'nae-helm)
-
-(require 'nae-ido)
-(require 'nae-bookmark)
-(require 'nae-hippie-expand)
-(require 'nae-windows)
-(require 'nae-sessions)
-(require 'nae-git)
-(require 'nae-vi)
-
-(require 'nae-autopair)
-(require 'nae-editing-utils)
-
-(require 'nae-web-mode)
-(require 'nae-c)
-(require 'nae-php)
-(require 'nae-org)
-(require 'nae-slime)
-(require 'nae-emmet)
-(require 'nae-vimrc-mode)
-(require 'nae-thrift)
-
-(require 'nae-shell)
-;; (require 'nae-w3m)
-(require 'nae-rss)
-(require 'nae-douban-fm)
-(require 'nae-calendar)
-
-(require 'nae-misc)
-(require 'nae-dir-locals)
-
-(require 'nae-key-bindings)
-
-(require 'nae-hack)
-
-(require 'nae-locales)
+(load "nae-modules")
 
 ;; 开启server
 (require 'server)
@@ -80,4 +29,4 @@
   (load custom-file))
 
 ;; 本地配置
-(require 'nae-local nil t)
+(require 'local nil t)
