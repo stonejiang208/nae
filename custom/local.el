701e533c6e8ca2cfffd350ff2e4156813fba0454
hellobear <i@hellobear.net>
2014-03-03 08:03:25 +0800

    修改邮箱

--- custom/local.el
+++ custom/local.el
@@ -1,5 +1,5 @@
 (setq user-full-name "hellobear")
 (setq user-login-name "hellobear")
-(setq user-mail-address "hellobear@foxmail.com")
+(setq user-mail-address "i@hellobear.net")
 
 (provide 'local)
