cb68808f2f7289e72f47da1cccc409b54d2c6737
hellobear <i@hellobear.net>
2014-03-03 08:03:06 +0800

    配置org

--- modules/nae-org.el
+++ modules/nae-org.el
@@ -54,45 +54,44 @@
 
 (require 'ox-publish)
 
-(setq org-html-postamble-format (quote (("zh-CN" "<p class=\"license\">本文采用 <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/3.0/cn/\">知识共享署名-相同方式共享 3.0 中国大陆许可协议</a> 进行许可</p><p class=\"date\">%d</p>"))))
-(setq org-html-preamble nil)
+;; (setq org-html-postamble-format (quote (("zh-CN" "<p class=\"license\">本文采用 <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/3.0/cn/\">知识共享署名-相同方式共享 3.0 中国大陆许可协议</a> 进行许可</p><p class=\"date\">%d</p>"))))
+(setq org-html-postamble-format '(("zh-CN" "<p class=\"author\">作者：%a (%e)</p>\n<p class=\"date\">日期：%d</p>")))
+(setq org-html-preamble t)
 (setq org-html-postamble nil)
 (setq org-src-fontify-natively t)
 (setq org-publish-sitemap-sort-files 'anti-chronologically)
-
+(setq org-html-head-include-default-style nil)
+(setq org-html-head-include-scripts nil)
 
 ;; 生成的html不附上xml声明
 (setq org-html-xml-declaration nil)
 
 (setq org-publish-project-alist
-      '(("org"
-         :author "dongxiong"
+      '(("blog"
+         :author "hellobear"
+         :email "i@hellobear.net"
          :language "zh-CN"
-         :base-directory "~/Dropbox/blog/org/"
+         :base-directory "~/Dropbox/mysite/web/blog"
          :base-extension "org"
+         :auto-sitemap t
          :sitemap-filename "sitemap.org"
          :sitemap-title "Sitemap"
          :sitemap-file-entry-format "%t"
-         :publishing-directory "~/Dropbox/blog/html/"
          :publishing-function org-html-publish-to-html
+         :publishing-directory "~/Dropbox/mysite/gen/blog"
          :htmlized-source t
          :recursive t
          :headline-levels 4
-         :auto-sitemap t
-         :html-link-up "/sitemap.html"
+         :html-link-up "/blog/sitemap.html"
          :html-link-home "/"
-         :html-head "<meta http-equiv=\"pragma\" content=\"no-cache\" />\n<link rel=\"stylesheet\" type=\"text/css\" href=\"/style.css\" />"
-         :html-head-include-default-style nil
-         :html-head-include-scripts nil
-         )
+         :html-head "<meta http-equiv=\"pragma\" content=\"no-cache\" />\n<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/blog.css\" />"
+         :html-postamble t)
         ("static"
-         :base-directory "~/Dropbox/blog/org/"
+         :base-directory "~/Dropbox/mysite/web"
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|ico\\|pdf\\|mp3\\|ogg\\|swf\\|html"
-         :publishing-directory "~/Dropbox/blog/html/"
-         :recursive t
          :publishing-function org-publish-attachment
-         )
-        ("blog" :components ("org" "static"))))
+         :publishing-directory "~/Dropbox/mysite/gen"
+         :recursive t)))
 
 (provide 'nae-org)
 
