614c83d92454f1fa765fe7219fa43aa86e3d2b8c
hellobear <i@hellobear.net>
2014-03-24 09:10:17 +0800

    默认主题用回monokai

--- modules/nae-themes.el
+++ modules/nae-themes.el
@@ -34,7 +34,7 @@
 (nae-require-package 'zenburn-theme)
 
 ;; 默认主题 （monokai、sanityinc-inc-solarized-light、sanityinc-solarized-dark）
-(setq-default custom-enabled-themes '(zenburn))
+(setq-default custom-enabled-themes '(monokai))
 
 ;; 确保主题在没有配置主题的情况下也能启用
 (defun reapply-themes ()
